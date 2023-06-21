eab76ae6d33a6a9628ad75be6ce8085f19799533
EricWithC04 <negativegame117@gmail.com>
2023-06-21 14:53:49 -0300

    model tabla de reservas

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,1 +1,29 @@
 // TODO: Crear modelo de datos de Reserva
+const { DataTypes, DATEONLY } = require("sequelize");
+
+module.exports = (sequelize) => {
+    sequelize.define('reserva', {
+        cod_reserva: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        surname: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false
+        }
+    })
+}
